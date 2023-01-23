0f588ed4d71bdb39967fc43d7e18afb8f0231db2
Milan Bhandari <103623160+milanbhandari047@users.noreply.github.com>
2023-01-23 17:02:54 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+var convert = document.getElementById("convert"),
+
+   voiceIco = document.getElementById("voiceIco"),
+
+  speech = document.getElementById("inputText"),
+count = 1;
+
+speech.addEventListener("change", function(){
+    speechText = this.ariaValueMax;
+
+    speechSynthesis.cancel();
+
+    convert.innerHTML = "Text to Speech";
+    voiceIco.innerHTML = "🔊 ";
+
+});
+
+convert.addEventListener("click",function(){
+    if(!speechSynthesis.speaking || speechSynthesis.pause()){
+        speechText = speech.value;
+
+        var speechVoice = new SpeechSynthesisUtterance();
+        var voices = speechSynthesis.getVoices();
+        speechVoice .voice = voices[2];
+        speechVoice.text = speechText;
+        speechVoice.lang = "en-US";
+        speechSynthesis.speak(speechVoice);
+    }
+
+
+if ( count == 1)
+{
+    convert.innerHTML = "Play";
+    voiceIco.innerHTML = "🔊";
+    speechSynthesis.resume();
+    setTimeout(()=>{
+        count = 2 ;
+
+    },300);
+
+}
+else{
+    speechSynthesis.paused;
+    convert.innerHTML = "Pause";
+    voiceIco.innerHTML = "🔊";
+    count = 1;
+}
+
+setInterval(() => {
+    if(!speechSynthesis.speaking && count == 2){
+        convert.innerHTML = "Text to Speech";
+        voiceIco.innerHTML = "🔊";
+        count = 1;
+        console.log(count);
+
+    }
+}, 100);
+
+
+})
